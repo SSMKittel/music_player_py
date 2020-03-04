8d88dbcf6964c2a6eb39dbed9ea896b9c44013f9
Samuel Kittel <S.S.M.Kittel+github@gmail.com>
2020-03-04 21:32:23 +1030

    Add pi volume control

---
+++ volume-down.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from gpiozero import Button
+from time import sleep
+
+import os
+button = Button(20)
+while True:
+    button.wait_for_press()
+    os.system("amixer -q -M sset PCM 1%-")
+    sleep(0.2)
